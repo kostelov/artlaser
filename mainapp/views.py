2d996022055cdf15ae7b0a0329f3513b29b792d5
nkostelov <nkostelov@gmail.com>
2018-03-20 00:24:00 +0200

    ================ ДЗ_2 ================ - создан базовый шаблон - реализовано наследование - основное меню подключено как подшаблон (include) - меню каталога подключено как подшаблон (include) - реализована работа со статическими файлами - на странице контактов динамически подгружаем карту и контактные данные - год в футере будет меняться атоматически - сделаны динамические urls - сделана подсветка активного пункта основного меню средствами django
    !!! ---- проблемы ---- !!!
    - пропала возможность нажать на пункт основго меню (нет ссылки)
    [РЕШЕНО] поменял местами href и class, ссылка на адрес страницы должна быть на первом месте, затем стили

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,12 +1,51 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 def main(request):
-    return render(request, 'mainapp/index.html')
+    title = 'Главная'
+    context = {
+        'title': title,
+    }
+    return render(request, 'mainapp/index.html', context)
+
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    title = 'Каталог'
+    context = {
+        'title': title,
+    }
+    return render(request, 'mainapp/products.html', context)
+
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    title = 'Контакты'
+    locations = [
+        {
+            'map': 'https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d2525.797238591331!2d25.330129232289313!3d50.72370072838036!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2sua!4v1521399079914',
+            'country': 'Украина',
+            'phone': '+380 (96) 890-77-11',
+            'email': 'office&@art-laser.com.ua',
+            'address': 'Рованцы',
+            },
+        {
+            'map': 'https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d2122.489204755704!2d25.35729590891547!3d50.755563921938034!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2sua!4v1521399361082',
+            'country': 'Украина',
+            'phone': '+380 (95) 908-77-55',
+            'email': 'office&@art-laser.com.ua',
+            'address': 'г. Луцк, ул. Сухомлинского, 1',
+            },
+        {
+            'map': 'https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d2122.3178619805512!2d25.352685748786712!3d50.75934212624163!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2sua!4v1521399610372',
+            'country': 'Украина',
+            'phone': '+380 (98) 832-70-60',
+            'email': 'office&@art-laser.com.ua',
+            'address': 'г. Луцк, ул. Конякина, 1',
+            },
+    ]
+    context = {
+        'title': title,
+        'locations': locations,
+    }
+    return render(request, 'mainapp/contact.html', context)
