9da0f6bc486ba12b9dc975f12d6bcd9801690fdd
nkostelov <nkostelov@gmail.com>
2018-03-23 23:32:42 +0200

    - в настройках прописываем путь к файлам media - созданы модели для категорий и товаров

--- artlaser/urls.py
+++ artlaser/urls.py
@@ -17,9 +17,15 @@ from django.contrib import admin
 from django.urls import path
 import mainapp.views as mainapp
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('', mainapp.main, name='main'),
     path('products/', mainapp.products, name='products'),
     path('contact/', mainapp.contact, name='contact'),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+
 # Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(verbose_name='Категория', max_length=64, unique=True)
+    description = models.TextField(verbose_name='Описание категории', blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    name = models.CharField(verbose_name='Название товара', max_length=128)
+    vendor_id = models.CharField(verbose_name='Артикул', max_length=10, blank=True)
+    image = models.ImageField(upload_to='products_image', blank=True)
+    short_descript = models.CharField(verbose_name='Краткое описание', max_length=64, blank=True)
+    description = models.TextField(verbose_name='Описание товара', blank=True)
+    price = models.DecimalField(verbose_name='Цена товара', max_digits=8, decimal_places=2, default=0)
+    quantity = models.PositiveIntegerField(verbose_name='Кол-во на складе', default=0)
+
+    def __str__(self):
+        return '{} ({})'.format(self.name, self.category)
