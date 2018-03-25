7d6d222dc7692157aad685d7c0284c80a20cb5d2
nkostelov <nkostelov@gmail.com>
2018-03-25 21:49:19 +0300

    - создан диспетчер url для основного приложения (пока прописан только для каталога)

---
+++ mainapp/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import re_path
+import mainapp.views as mainapp
+
+app_name = 'mainapp'
+
+urlpatterns = [
+    re_path(r'^$', mainapp.products, name='index'),
+]
